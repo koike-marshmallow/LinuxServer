f0fa6edfbec702d8c3b093aa0b306d74eaa9e3b4
takaaki koike <taka.vao.321@gmail.com>
2015-12-22 20:58:33 +0900

    各種ツールを追加

---
+++ sh/backup/tools/archive.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mv -v backup*.txt archive
---
+++ sh/spindown/tools/show_log.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+date_str=`date '+%y%m%d'`
+today_file_name="hdd_spindown${date_str}.txt"
+
+if [ $2 ]
+then
+	select_file=$2
+else
+	select_file=$today_file_name
+fi
+
+if [ $1 ]
+then
+	case $1 in
+	"follow" )
+		tail -f $today_file_name
+	;;
+	"chkspin" )
+		grep -E "(SPINUP|SPINDOWN|initialization)" $select_file
+	;;
+	"statechg" )
+		grep "chg:YES" $select_file
+	;;
+	* )
+		echo "未定義のコマンド: $1"
+	;;
+	esac
+else
+	tail $today_file_name
+fi
