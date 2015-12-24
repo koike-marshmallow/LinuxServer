6d219ab26cd167a9526abee1eba8ff0824f87199
taka <taka@example.com>
2015-12-24 22:36:18 +0900

    raspi-ubuntuの設定ファイルを追加

---
+++ raspi/configs/usr_local_bin/backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+datestr=`date '+%y%m%d%H%M%S'`
+log_file_dest="/home/ubuntu/log/backup/backup${datestr}.txt"
+
+##logger
+print_log()
+{
+	echo $@ >> $log_file_dest
+}
+
+
+##configuation
+backup_dest=/mnt/nas_hdd1/NAS_BACKUP/  #スラッシュをつけること
+sources=(/var/smb/test /var/smb/safety)
+
+
+##mainscript
+
+datestr=`date '+%c'`
+
+print_log "--------------------"
+print_log "--------------------"
+print_log "BACKUP PROCESS START (${datestr})"
+print_log "backup_dest: ${backup_dest}"
+print_log "sources: ${sources[@]}"
+
+
+for src in ${sources[@]}
+do
+	if [ "$1" = "run" ]
+	then
+		print_log ""
+		print_log "---------------"
+		print_log "----- start backup \"${src}\""
+		rsync $src $backup_dest -av --delete >> $log_file_dest 2>&1
+	else
+		print_log ""
+		print_log "---------------"
+		print_log "----- backup dry run \"${src}\""
+		rsync $src $backup_dest -avn --delete >> $log_file_dest 2>&1
+	fi
+done
+
+print_log ""
+print_log "--------------"
+print_log "----- backup complete."
+
+datestr=`date '+%c'`
+print_log "BACKUP PROCESS END (${datestr})"
