09cdddc2f3d010b8887875b8419d18bfd93f4b47
takaaki koike <taka@example.com>
2015-12-30 00:19:32 +0900

    index.html等を変更

--- www-service/sh/spindownlog/update_spindownlog.sh
+++ www-service/sh/spindownlog/update_spindownlog.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #configure
-dest_file="/var/www/html/spindownlog_latest.txt"
+dest_file="/var/www/html/spindownlog_latest.html"
 copy_rows_count=30
 
 
@@ -12,15 +12,22 @@ log_file_path="${log_file_dir}hdd_spindown${datestr}.txt"
 
 
 #prepare destination file
-if [ ! -f $dest_file ]
-then
-	touch $dest_file
-fi
+: > $dest_file
 
+#write html header
+echo "<html>" >> $dest_file
+echo "<head><meta charset=\"utf-8\"/></head>" >> $dest_file
 
-#write copy log file
-echo "FILE NAME: ${log_file_path}" > $dest_file
+#write log record(body)
+echo "<body><pre><code>" >> $dest_file
+
+echo "FILE NAME: ${log_file_path}" >> $dest_file
 echo "UPDATE TIME: " `date` >> $dest_file
 echo "------------------------------" >> $dest_file
 
 tail -n $copy_rows_count $log_file_path >> $dest_file
+
+echo "</code></pre></body>" >> $dest_file
+
+#write html footer
+echo "</html>" >> $dest_file
