3c4024dd6d38475d2716b499062316070f6b9d12
takaaki koike <taka@example.com>
2015-12-30 23:52:04 +0900

    hddsizeをhtmlに移行

--- www-service/sh/hddsize/update_hddsize.sh
+++ www-service/sh/hddsize/update_hddsize.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 dest_file="/var/www/html/hdd_size_info.txt"
+html_dest_file="/var/www/html/hdd_size_info.html"
 
 : > $dest_file
 
@@ -9,3 +10,5 @@ du -s /mnt/nas_hdd1/* | sort -n >> $dest_file
 
 echo "(last modified " `date` " )" >>$dest_file
 echo "---------------------------" >>$dest_file
+
+/usr/local/bin/htmlconvert.sh $dest_file $html_dest_file
