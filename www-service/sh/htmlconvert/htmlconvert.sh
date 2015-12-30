088afe89d45b534d7eed2f85f8e4111e63d8a4e7
takaaki koike <taka@example.com>
2015-12-30 22:57:01 +0900

    html形式に変換するshを作成

---
+++ www-service/sh/htmlconvert/htmlconvert.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+htmlout()
+{
+	echo $@ >> $output_file
+}
+
+
+if [ "$1" ]
+then
+	input_file=$1
+else
+	echo "入力ファイルを指定してください"
+fi
+
+if [ "$2" ]
+then
+	output_file=$2
+else
+	output_file="output.html"
+fi
+
+if [ "$3" ]
+then
+	html_title=$3
+else
+	html_title=$1
+fi
+
+
+#write html header
+htmlout "<html>"
+htmlout 	"<head>"
+htmlout 		"<title>${html_title}</title>"
+htmlout 		"<meta charset=\"utf-8\"/>"
+htmlout 	"</head>"
+
+#write body
+htmlout "<body>"
+htmlout "<pre><code>"
+cat $input_file >> $output_file
+htmlout "</pre></code>"
+htmlout "</body>"
+
+#write html footer
+htmlout "</html>"
